0e998ef744bad73f11cddacd3a41fb3ae3b2c4c6
mountainstar <chriswirthdesign@gmail.com>
2016-09-11 10:50:00 -0600

    Formatted Date

--- src/App.js
+++ src/App.js
@@ -7,46 +7,54 @@ import './App.css';
 import IssuesContainer from './containers/IssuesContainer';
 
 class App extends Component {
-constructor(){
-    super();
-    this.state = {};
-  }
+    constructor(){
+        super();
+        this.state = {};
+    }
 
-  componentWillMount(){
-    this.getIssues();
-  }
-  render() {
-    let allIssues = _.map(this.state.allIssues, (issue) => {
-      return <li key={issue.id}>
-               <strong>{issue.title}</strong>
-               <a href={issue.user.url} >
-                 <img className="avatar-small" src={issue.user.avatar_url} alt={issue.user.login}/>
-               </a>
-               <p>
-                 {issue.body}
-               </p>
-                 <span className="status" data-status={issue.state}>{issue.state}</span>
-                 <span> Created: {issue.created_at}</span><br />
-             </li>
-    });
+    componentWillMount(){
+        this.getIssues();
+    }
+    render() {
 
-    return (
-      <div>
-        <ol className="timeline group">
-          {allIssues}
-        </ol>
-        <IssuesContainer />
-      </div>
-    );
-  }
-  getIssues(query = "roadmap") {
-    let issues = `https://api.github.com/repos/pebutler3/${query}/issues?state=all`;
-    axios.get(issues).then((response) => {
-      this.setState({
-        allIssues: response.data,
-      });
-    });
-  };
+
+        let allIssues = _.map(this.state.allIssues, (issue) => {
+            /*Date format*/
+            var cts = issue.created_at,
+                cdate = (new Date(cts)).toLocaleString();
+
+            return <li key={issue.id}>
+                <strong>{issue.title}</strong>
+                <a href={issue.user.url} >
+                    <img className="avatar-small" src={issue.user.avatar_url} alt={issue.user.login}/>
+                </a>
+                <p>
+                    {issue.body}
+                </p>
+                <span className="status" data-status={issue.state}>{issue.state}</span>
+                <span> Created: { cdate }</span>
+                <br />
+            </li>
+        });
+
+        return (
+            <div>
+                <ol className="timeline group">
+                    {allIssues}
+                </ol>
+                <IssuesContainer />
+            </div>
+        );
+    }
+    getIssues(query = "roadmap") {
+        let issues = `https://api.github.com/repos/pebutler3/${query}/issues?state=all`;
+        axios.get(issues).then((response) => {
+            this.setState({
+                allIssues: response.data,
+            });
+        });
+    };
+
 }
 
 
