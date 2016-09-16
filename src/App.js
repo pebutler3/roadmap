01156b12aac1c3d50454e7f285833c4f26a487a8
pebutler3 <p.e.butler3@gmail.com>
2016-09-15 23:53:15 -0600

    got basic filtering of issues to render properly on clicks // added basic styles for test

--- src/App.js
+++ src/App.js
@@ -10,69 +10,92 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
+          allIssues: '',
           urlState: 'all'
-        };
+        }
+        this.urlStateToAll = this.urlStateToAll.bind(this);
+        this.urlStateToOpen = this.urlStateToOpen.bind(this);
+        this.urlStateToClosed = this.urlStateToClosed.bind(this);
     }
 
-    setUrlStateToOpen(){
-      this.setState({
-        urlState: 'open'
-      })
+    urlStateToAll() {
+      this.getAllIssues();
     }
 
-    testFunc() {
-      console.log('made it!')
+    urlStateToOpen() {
+      this.getOpenIssues();
     }
 
-    componentWillMount(){
-        this.getIssues();
+    urlStateToClosed() {
+      this.getClosedIssues();
+    }
+
+    componentWillMount() {
+        this.getAllIssues();
     }
 
     render() {
-        let allIssues = _.map(this.state.allIssues, (issue) => {
+            let allIssues = _.map(this.state.allIssues, (issue) => {
             // Date format
             let cts = issue.created_at,
                 cdate = (new Date(cts)).toLocaleString();
 
             return <li key={issue.id}>
-                <strong>{issue.title}</strong>
-                <a href={issue.user.url} >
-                    <img className="avatar-small" src={issue.user.avatar_url} alt={issue.user.login}/>
-                </a>
-                <p>
-                    {issue.body}
-                </p>
-                <span className="status" data-status={issue.state}>{issue.state}</span>
-                <span> Created: { cdate }</span>
-                <br />
-            </li>
+                      <strong>{issue.title}</strong>
+                      <a href={issue.user.url} >
+                          <img className="avatar-small" src={issue.user.avatar_url} alt={issue.user.login}/>
+                      </a>
+                      <p>
+                          {issue.body}
+                      </p>
+                      <span className="status" data-status={issue.state}>{issue.state}</span>
+                      <span> Created: { cdate }</span>
+                      <br />
+                  </li>
         });
-
-        // let allIssues = {this.state.allIssues, (issue) {
-        //                   return <li>
-        //                 })}
 
         return (
             <div>
-                <button className="open" onClick={this.setUrlStateToOpen.bind(this)}>Open</button>
-                <button className="open" onClick={this.testFunc}>test</button>
+                <div className="filters">
+                <h1>{this.state.urlState.toUpperCase()}</h1>
+                <button className="all" onClick={this.urlStateToAll}>All</button>
+                <button className="open" onClick={this.urlStateToOpen}>Open</button>
+                <button className="closed" onClick={this.urlStateToClosed}>Closed</button>
+                </div>
                 <ol className="timeline group">
                     {allIssues}
                 </ol>
-                <h1>{this.state.urlState}</h1>
                 <IssuesContainer />
             </div>
         );
     }
-    getIssues(query = "roadmap", userTest='pebutler3') {
-        let issues = 'https://api.github.com/repos/' + userTest + '/' + query + '/issues?state=' + this.state.urlState;
+    getAllIssues(query = 'roadmap', userTest='pebutler3', urlState='all') {
+        let issues = 'https://api.github.com/repos/' + userTest + '/' + query + '/issues?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&state=' + urlState;
         axios.get(issues).then((response) => {
             this.setState({
                 allIssues: response.data,
+                urlState: 'all'
             });
-            console.log(response.data)
         });
     };
+    getOpenIssues(query = 'roadmap', userTest = 'pebutler3', urlState = 'open') {
+      let openIssues = 'https://api.github.com/repos/' + userTest + '/' + query + '/issues?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&state=' + urlState;
+      axios.get(openIssues).then((response) => {
+        this.setState({
+          allIssues: response.data,
+          urlState: 'open'
+        });
+      });
+    };
+    getClosedIssues(query = 'roadmap', userTest = 'pebutler3', urlState = 'closed') {
+      let closedIssues = 'https://api.github.com/repos/' + userTest + '/' + query + '/issues?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&state=' + urlState;
+      axios.get(closedIssues).then((response) => {
+        this.setState({
+          allIssues: response.data,
+          urlState: 'closed'
+        });
+      });
+    }
 
 }
 
